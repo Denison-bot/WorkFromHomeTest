5afe52eed995b89a7ea72f9783b1ecf0cc7f03f1
Denison Percey <denisonipercey@outlook.com>
2022-01-10 13:48:31 -0400

    Fixed file

---
+++ WorkingFromHomeTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingFromHomeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My PC is perfectly fine to work from,");
+            Console.WriteLine("I've done most of my work from my rig");
+            Console.ReadKey();
+        }
+    }
+}
